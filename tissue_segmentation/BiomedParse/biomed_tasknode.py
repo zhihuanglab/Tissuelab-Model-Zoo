2a6c0f28b4c8cc236c88bb48700003464912f1fe
Songhao-LI <sl10500@nyu.edu>
2025-03-16 18:10:32 -0400

    [update]:area size checking

--- tissue_segmentation/BiomedParse/biomed_tasknode.py
+++ tissue_segmentation/BiomedParse/biomed_tasknode.py
@@ -49,14 +49,14 @@ def binary_mask_to_polygons(binary_mask):
     polygons = []
     for contour in contours:
         polygon = contour.reshape(-1, 2).tolist()
-        if len(polygon) >= 10:  # Only add polygons with at least 10 points
+        if len(polygon) >= 10 and cv2.contourArea(contour) >= 10000:
             polygons.append(polygon)
     return polygons
 
 def binary_mask_to_contours(binary_mask):
     contours, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    filtered_polygons = [contour for contour in contours if len(contour) >= 10]
+    filtered_polygons = [contour for contour in contours if len(contour) >= 10 and cv2.contourArea(contour) >= 10000]
 
     if not filtered_polygons:
         return np.zeros((0, 10, 2), dtype=np.int32)
@@ -562,6 +562,16 @@ def execute_node(background_tasks: BackgroundTasks):
 
             # 3) polygons on bigmask and convert to absolute coordinates
             polygons = binary_mask_to_polygons(bigmask)
+            
+            print("area size:")
+            total_area = 0
+            for i, poly in enumerate(polygons):
+                contour = np.array(poly).reshape((-1, 1, 2)).astype(np.int32)
+                area = cv2.contourArea(contour)
+                print(f"  polygon #{i+1}: {area:.2f} pixels")
+                total_area += area
+            print(f"tital area: {total_area:.2f} pixels")
+            
             # Convert to absolute coordinates by adding bbox offset
             absolute_polygons = [
                 [[x + BBOX[0], y + BBOX[1]] for x, y in polygon]
@@ -574,7 +584,7 @@ def execute_node(background_tasks: BackgroundTasks):
                 "contours": absolute_polygons,
                 "bbox": BBOX
             }
-            print(result_value)
+            # print(result_value)
         else:
             # normal PNG
             if IMAGE_ARR is None:
@@ -603,7 +613,7 @@ def execute_node(background_tasks: BackgroundTasks):
             out_str = json.dumps(result_value, ensure_ascii=False)
             hf.create_dataset(out_path, data=out_str.encode("utf-8"))
 
-            print(f"[DEBUG] => wrote JSON to {out_path}: {out_str}")
+            # print(f"[DEBUG] => wrote JSON to {out_path}: {out_str}")
             try:
                 with h5py.File(H5_PATH, "r") as hf:
                     print("[DEBUG] H5 top-level keys:", list(hf.keys()))
