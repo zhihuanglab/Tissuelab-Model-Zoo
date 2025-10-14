b59412b0cd73b4c1d8ade04ca2d650f82d689069
Songhao-LI <sl10500@nyu.edu>
2025-10-14 14:34:50 -0400

    [remove]:unused models

--- nuclei_segmentation/CellViT++/CellViT-plus-plus/cellvit/training/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Cell Segmentation and detection using our cellvit model
-#
-# @ Fabian Hörst, fabian.hoerst@uk-essen.de
-# Institute for Artifical Intelligence in Medicine,
-# University Medicine Essen
--- nuclei_segmentation/CellViT++/PathoPatcher/examples/dicom_conversion_examples.sh
+++
@@ -1,6 +0,0 @@
-wsidicomizer \
-    -i ./test_database/3DHistech/Orig/E6225_21-1A.11.mrxs \
-    -o ./test_database/3DHistech/Converted \
-    -w 8 \
-    --format jpeg \
-    --quality 95
