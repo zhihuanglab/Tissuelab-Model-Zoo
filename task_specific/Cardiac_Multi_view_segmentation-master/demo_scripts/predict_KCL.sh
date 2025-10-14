fbcb0d37d3053f82f820b753995659e6e8613463
Songhao-LI <sl10500@nyu.edu>
2025-10-14 14:28:29 -0400

    [update]:ark plus

--- task_specific/Cardiac_Multi_view_segmentation-master/demo_scripts/predict_KCL.sh
+++
@@ -1,3 +0,0 @@
-python predict.py --sequence LVSA --root_dir '/vol/medic02/users/cc215/data/KCL_SmartHeart_processed/sa_4d_split' \
---image_format 'sa_cine_{}.nii.gz' --save_folder_path '/vol/medic02/users/cc215/data/KCL_SmartHeart_processed/Unet_predict' --save_name_format 'seg_sa_cine_{}.nii.gz' 
-
